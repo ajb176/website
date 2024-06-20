57e4b315d1631f35a23868326ce5a359928ca5e2
ajb176 <bennettajay@gmail.com>
2024-06-21 00:02:13 +0530

    .

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -1,4 +1,4 @@
-// 
+// Importing function that returns true/false depending on whether user is found on team
 const isMemberOf = require('../utils/check-team-membership');
 const commentContent = 'You must be a member of the HFLA website team in order to create pull requests. Please see our page on how to join us as a member at HFLA: https://www.hackforla.org/getting-started.  If you have been though onboarding, and feel this message in error, please message us in the #hfla-site team slack channel with the link to this PR.';
 
