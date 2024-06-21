e6f80302af52f743e3f3812361cf063523949986
ajb176 <bennettajay@gmail.com>
2024-06-21 11:58:34 +0530

    l

--- github-actions/utils/check-team-membership.js
+++ github-actions/utils/check-team-membership.js
@@ -5,7 +5,7 @@
 
 - Returns true or false depending on whether the username is found on the passed team, 404 means the user passed wasn't 
 found on the team passed. Any other type of error will be thrown.
-- Need admin:orgs permission to use this function, the least permissive token which contains this is the secrets.TEAM token.
+- Need admin:orgs permission to use this function, the least permissive token which contaidns this is the secrets.TEAM token.
 Lack of permission will result in a 403 error.
 - The method of obtaining the github username will vary depending on the contents of the context object. See github action 
 docs on printing context information into the log.
