9da2f11c1962dd064f2ae6907eed2797fd2c3ff6
ajb176 <bennettajay@gmail.com>
2024-09-02 12:57:51 +0530

    Log Dump

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -5,6 +5,8 @@ async function main({github,context}) {
     const prAuthor = context.payload.sender.login;
     const prNumber = context.payload.number;
     const isMember = await isMemberOfTeam(github, prAuthor, 'website-write');
+    console.log(github);
+    console.log(context);
     if (isMember) {    
         console.log('Successfully verified!');
     }
