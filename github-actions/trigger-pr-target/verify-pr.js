ba670df6d0dff9a6fad86d246a3cc9ba34f70143
ajb176 <bennettajay@gmail.com>
2024-09-02 15:28:24 +0530

    test

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -5,8 +5,8 @@ async function main({github,context}) {
     const prAuthor = context.payload.sender.login;
     const prNumber = context.payload.number;
     const isMember = await isMemberOfTeam(github, prAuthor, 'website-write');
-    if (isMember || prAuthor == 'dependabot[bot]') {    
-        console.log('Successfully verified!');
+    if (isMember || (prAuthor == 'dependabot[bot]')) {    
+        console.log('Successfully verified!'); //j
     }
     else {
         try {
