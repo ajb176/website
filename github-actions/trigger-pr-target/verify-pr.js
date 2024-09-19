a84ad4913c4a41c817c5041a60426e39d6e65c65
ajb176 <bennettajay@gmail.com>
2024-09-19 15:13:54 +0530

    Checking Context Varialbes

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -5,11 +5,11 @@ If you have been though onboarding, and feel this message was sent in error, ple
 #hfla-site team Slack channel with the link to this PR.';
 
 async function main({github,context}) {
-    console.log(context.payload.base);
-    console.log(context.payload.head);
+    console.log(context.payload.pull_request.base);
+    console.log(context.payload.pull_request.head);
     const prAuthor = context.payload.sender.login;  
     const prNumber = context.payload.number;
-    const repoFullName = context.payload.repository.full_name; //string: '<repo_owner>/<repo_name>'
+    const repoFullName = context.payload.repository.full_name; //string: '<repo_owner>/<repo_name>'d
     const ownerRepo = repoFullName.split("/");
     const isMember = await isMemberOfTeam(github, prAuthor, 'website-write');
     if (isMember || prAuthor =='dependabot[bot]') {    
